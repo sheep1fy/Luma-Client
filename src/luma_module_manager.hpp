639018cdb93fa37b3bf32d6855c4fa764af6f3ef
Sheepify <felixriley523@gmail.com>
2026-01-31 10:25:21 +1100

    Update luma_module_manager.hpp

--- src/luma_module_manager.hpp
+++ src/luma_module_manager.hpp
@@ -1,8 +1,9 @@
+#pragma once
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// These functions can be called from luma_linux.c
 void init_luma_manager();
 void toggle_luma_menu();
 void run_luma_tick();
