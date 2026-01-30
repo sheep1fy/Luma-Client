17ba879a048b8d8b5bb4fd76d6a628a6f4363bca
Sheepify <felixriley523@gmail.com>
2026-01-31 10:22:03 +1100

    Create luma_module_manager.hpp

---
+++ src/luma_module_manager.hpp
@@ -0,0 +1,12 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// These functions can be called from luma_linux.c
+void init_luma_manager();
+void toggle_luma_menu();
+void run_luma_tick();
+
+#ifdef __cplusplus
+}
+#endif
