fef203d2154a65eec3ac6b0ddb25d07921e1a1ff
Sheepify <felixriley523@gmail.com>
2026-01-30 16:31:17 +1100

    Update luma_ping_module.cpp

--- src/modules/luma_ping_module.cpp
+++ src/modules/luma_ping_module.cpp
@@ -4,22 +4,22 @@
 #include "imgui.h"
 #include <cstdio>
 
-static LumaVec3 g_pos = {0, 64, 0};
+static int g_ping = 0;
 
-static auto LumaCoordsTick = [](float) {
-    g_pos = LumaGetPlayerPosition();
+static auto LumaPingTick = [](float) {
+    g_ping = LumaGetPing();
 };
 
-static auto LumaCoordsRender = []() {
-    char buf[64];
-    snprintf(buf, sizeof(buf), "X:%.1f Y:%.1f Z:%.1f", g_pos.x, g_pos.y, g_pos.z);
+static auto LumaPingRender = []() {
+    char buf[32];
+    snprintf(buf, sizeof(buf), "%d ms", g_ping);
     
     ImDrawList* dl = ImGui::GetBackgroundDrawList();
-    ImVec2 pos = g_lumaCoordsHud.pos;
+    ImVec2 pos = g_lumaPingHud.pos;
     dl->AddText(pos + ImVec2(1,1), IM_COL32(0,0,0,200), buf);
-    dl->AddText(pos, IM_COL32(0,255,0,255), buf);
+    dl->AddText(pos, IM_COL32(255,255,0,255), buf);
 };
 
-LumaModule LumaCreateCoordsModule() {
-    return {"Coords", true, LumaCoordsTick, LumaCoordsRender};
+LumaModule LumaCreatePingModule() {
+    return {"Ping", true, LumaPingTick, LumaPingRender};
 }
