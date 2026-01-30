0eaa72a761a6ef4097d23f876f31c8560b1eb2e0
Sheepify <felixriley523@gmail.com>
2026-01-30 15:48:33 +1100

    Update luma_config.cpp

--- src/luma_config.cpp
+++ src/luma_config.cpp
@@ -2,40 +2,33 @@
 #include "luma_hud.h"
 #include "luma_module_manager.h"
 #include <fstream>
-#include <nlohmann/json.hpp>
-using json = nlohmann::json;
+#include <string>
 
-static const char* CONFIG_PATH = "~/.local/share/mcpelauncher/luma_config.json";
+static const std::string CONFIG_PATH = "~/.local/share/mcpelauncher/luma_config.txt";
 
 void LumaConfigSave() {
-    json j;
+    std::ofstream file(CONFIG_PATH);
+    if (!file) return;
     
     // Save HUD positions
-    j["hud"]["fps"]["x"] = g_lumaFpsHud.pos.x;
-    j["hud"]["fps"]["y"] = g_lumaFpsHud.pos.y;
-    j["hud"]["cps"]["x"] = g_lumaCpsHud.pos.x;
-    // ... more HUD elements
+    file << "fps_x=" << g_lumaFpsHud.pos.x << "\n";
+    file << "fps_y=" << g_lumaFpsHud.pos.y << "\n";
     
-    // Save module states
     auto& mods = LumaGetModuleManager().getModules();
     for (auto& m : mods)
-        j["modules"][m.name] = m.enabled;
-    
-    std::ofstream file(std::string(CONFIG_PATH));
-    file << j.dump(2);
+        file << "module_" << m.name << "=" << m.enabled << "\n";
 }
 
 void LumaConfigLoad() {
-    std::ifstream file(std::string(CONFIG_PATH));
-    if (!file.good()) return;
+    std::ifstream file(CONFIG_PATH);
+    if (!file) return;
     
-    json j;
-    file >> j;
-    
-    // Load HUD positions
-    if (j.contains("hud")) {
-        g_lumaFpsHud.pos.x = j["hud"]["fps"]["x"];
-        g_lumaFpsHud.pos.y = j["hud"]["fps"]["y"];
-        // ... more HUD elements
+    std::string line;
+    while (std::getline(file, line)) {
+        if (line.find("fps_x=") == 0) {
+            g_lumaFpsHud.pos.x = std::stof(line.substr(6));
+        } else if (line.find("fps_y=") == 0) {
+            g_lumaFpsHud.pos.y = std::stof(line.substr(6));
+        }
     }
 }
