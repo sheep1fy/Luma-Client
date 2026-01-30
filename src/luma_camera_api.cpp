931eb8384006ca4d47c81bf1b5f627160761a8a0
Sheepify <felixriley523@gmail.com>
2026-01-30 15:14:09 +1100

    Update luma_camera_api.cpp

--- src/luma_camera_api.cpp
+++ src/luma_camera_api.cpp
@@ -1,13 +1,23 @@
-// src/luma_camera_api.cpp 
+// src/luma_camera_api.cpp
 #include "luma_camera_api.h"
+#include "../apis/bedrock_1_21_30.hpp"
 
-// TODO: wire to real camera FOV
-static float g_lumaFov = 70.f;
+static bool g_cameraReady = false;
+
+static void LumaInitCameraAPI() {
+    if (g_cameraReady) return;
+    g_cameraReady = bedrock_1_21_30::init();
+}
 
 float LumaGetCurrentFOV() {
-    return g_lumaFov;
+    LumaInitCameraAPI();
+    if (!g_cameraReady || !bedrock_1_21_30::is_ready())
+        return 70.f;
+    return bedrock_1_21_30::get_current_fov();
 }
 
 void LumaSetCurrentFOV(float fov) {
-    g_lumaFov = fov;
+    LumaInitCameraAPI();
+    if (g_cameraReady && bedrock_1_21_30::is_ready())
+        bedrock_1_21_30::set_current_fov(fov);
 }
