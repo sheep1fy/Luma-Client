d507b52b55c27123118eed7d5da353e2ee63a590
Sheepify <felixriley523@gmail.com>
2026-01-30 19:22:31 +1100

    Update luma_linux.c

--- src/luma_linux.c
+++ src/luma_linux.c
@@ -2,31 +2,21 @@
 #include <time.h>
 #include <unistd.h>
 
-static volatile int luma_counter = 0;
-
 __attribute__((constructor))
 void luma_init() {
     FILE *f = fopen("/tmp/luma_proof.txt", "w");
     fprintf(f, "LUMA LOADED %ld PID=%d\n", (long)time(NULL), getpid());
     fclose(f);
     
-    // FORCE console spam
+    // 10x FORCE spam - IMPOSSIBLE to miss!
     for(int i = 0; i < 10; i++) {
-        printf("[LUMA] 🎉 LOADED #%d - Press K for menu!\n", i);
+        printf("[LUMA #%d] 🎉 LOADED SUCCESSFULLY! K=Menu!\n", i);
     }
-    
-    luma_counter = 1;
-}
-
-// Simple K key check (game loop)
-void luma_check_keys() {
-    if (luma_counter > 0) {
-        printf("[LUMA] TICK %d\n", luma_counter++);
-        // TODO: K key detection here
-    }
+    fflush(stdout);  // FORCE output
 }
 
 __attribute__((destructor))
 void luma_exit() {
     printf("[LUMA] EXIT\n");
+    fflush(stdout);
 }
