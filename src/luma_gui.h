e016dcfcf7421c6f164a6c24c854c7bffd967cb5
Sheepify <felixriley523@gmail.com>
2026-02-01 16:52:07 +1100

    Create luma_gui.h

---
+++ src/luma_gui.h
@@ -0,0 +1,16 @@
+// luma_gui.h
+
+#ifndef LUMA_GUI_H
+#define LUMA_GUI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void luma_gui_render(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // LUMA_GUI_H
