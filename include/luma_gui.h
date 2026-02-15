797a55f7b110fb7ad694a67d3fe64bd7ded15017
Sheepify <felixriley523@gmail.com>
2026-02-15 15:10:33 +1100

    Update luma_gui.h

--- include/luma_gui.h
+++ include/luma_gui.h
@@ -1,4 +1,17 @@
 // include/luma_gui.h
 #pragma once
+
+#ifdef __cplusplus
+// C++ API
 void LumaGUIRender();
 extern bool LumaHudEditMode;
+
+extern "C" {
+#endif
+
+// C API wrapper
+void luma_gui_render(void);
+
+#ifdef __cplusplus
+}
+#endif
